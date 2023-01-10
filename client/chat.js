d270a3fb08809f19b8ba2f5b66e930d764b6626b
Sujin Jeon <mackerel0106@gmail.com>
2023-01-10 23:32:58 +0900

    Storing Connected Clients #9 close

--- client/chat.js
+++ client/chat.js
@@ -1,3 +1,16 @@
 console.log('chat.js file loaded');
 
+// By default, socket.io() connects to the host that served the page
+// So we don't have to pass the server url
 const socket = io.connect();
+
+// Prompt to ask user's name
+const username = prompt("Welcome! Please enter your name: ");
+
+// Emit event to server with the user's name
+socket.emit("new-connection", { username });
+
+// Captures welcome-message event from the server
+socket.on("welcome-message", (data) => {
+	console.log("received welcome-message >>", data);
+});
